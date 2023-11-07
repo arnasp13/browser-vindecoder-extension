05e7e7d389fca0d7b79e8edfd6b00d039d5c868e
Arnas <arnaspuidokas6@gmail.com>
2023-11-07 16:54:53 +0200

    add payment

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "decodeVIN",
-    title: "Decode VIN",
-    contexts: ["selection"],
-  });
-});
+importScripts("ExtPay.js"); // or `import` / `require` if using a bundler
+const extpay = ExtPay("vindecoder");
+extpay.startBackground(); // this line is required to use ExtPay in the rest of your extension
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "decodeVIN") {
-    const vin = encodeURIComponent(info.selectionText);
-    const url = chrome.runtime.getURL(`popup.html?vin=${vin}`);
-    chrome.tabs.create({ url });
+extpay.getUser().then((user) => {
+  if (user.paid) {
+    chrome.runtime.onInstalled.addListener(() => {
+      chrome.contextMenus.create({
+        id: "decodeVIN",
+        title: "Decode VIN",
+        contexts: ["selection"],
+      });
+    });
+
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+      if (info.menuItemId === "decodeVIN") {
+        const vin = encodeURIComponent(info.selectionText);
+        const url = chrome.runtime.getURL(`popup.html?vin=${vin}`);
+        chrome.tabs.create({ url });
+      }
+    });
+  } else {
+    extpay.openPaymentPage();
   }
 });
--- popup.js
+++ popup.js
@@ -1,86 +1,142 @@
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.vin) {
-    document.getElementById("vinInput").value = message.vin;
-    // Trigger the submit button click event to fetch and display the VIN details
-    document.getElementById("submitBtn").click();
-  }
-});
+function getCustomKey(key) {
+  const keyMap = {
+    brandName: "Make",
+    modelName: "Model",
+    modelId: "Model ID",
+    vin: "VIN",
+    // ... add other custom key mappings as needed
+  };
+
+  // Return the custom key if it exists in the map, otherwise return the original key
+  return keyMap[key] || key;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const vin = urlParams.get("vin");
-  if (vin) {
-    document.getElementById("vinInput").value = decodeURIComponent(vin);
-    document.getElementById("submitBtn").click();
-  }
-});
-
-// New code added here
-document.getElementById("vinInput").addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    const vin = document.getElementById("vinInput").value;
-    if (vin) {
-      document.getElementById("submitBtn").click();
+  // Listener for messages from the Chrome extension
+  chrome.runtime.onMessage.addListener((message) => {
+    if (message.vin) {
+      displayVinData(message.vin);
     }
-  }
-});
+  });
 
-document
-  .getElementById("submitBtn")
-  .addEventListener("click", async function () {
-    const vin = document.getElementById("vinInput").value;
+  // Event listener for the submit button
+  document
+    .getElementById("submitBtn")
+    .addEventListener("click", async function () {
+      const vin = document.getElementById("vinInput").value;
+      if (vin) {
+        displayVinData(vin);
+      }
+    });
+
+  // Event listener for the VIN input to accept the Enter key
+  document.getElementById("vinInput").addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      const vin = document.getElementById("vinInput").value;
+      if (vin) {
+        displayVinData(vin);
+      }
+    }
+  });
+
+  // Function to fetch and display VIN data
+  async function displayVinData(vin) {
     const resultsDiv = document.getElementById("resultsDiv");
 
-    if (!vin) {
-      resultsDiv.textContent = "Please enter a VIN.";
-      return;
-    }
-
     try {
-      const response = await axios.get(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinextended/${vin}?format=json`
-      );
+      // Set up the API request options
+      const options = {
+        method: "GET",
+        url: "https://cis-automotive.p.rapidapi.com/vinDecode",
+        params: { vin },
+        headers: {
+          "X-RapidAPI-Key":
+            "42ce5a5983msha5832eae9e62f15p1996a9jsneba1dc8291f1",
+          "X-RapidAPI-Host": "cis-automotive.p.rapidapi.com",
+        },
+      };
 
-      const data = response?.data?.Results;
+      // Perform the API requests
+      const decodeResponse = await axios.request(options);
 
-      if (!data || data.length === 0) {
-        resultsDiv.textContent = "No data found for the entered VIN.";
-        return;
+      // Process the responses
+      const vehicleData = decodeResponse?.data;
+      // const historyData = historyResponse?.data?.data;
+
+      // Clear previous results
+      resultsDiv.innerHTML = "";
+
+      // Build the results display HTML
+      let resultsHTML = "<h2>Vehicle Information</h2>";
+
+      if (vehicleData) {
+        resultsHTML += '<div class="vehicle-data">';
+
+        Object.keys(vehicleData).forEach((key) => {
+          if (
+            !key.includes("Error") &&
+            vehicleData[key] &&
+            key !== "data" &&
+            key !== "recallInfo" &&
+            key !== "cacheTimeLimit"
+          ) {
+            resultsHTML += `<p><strong>${getCustomKey(key)
+              .replace(/([A-Z])/g, " $1")
+              .trim()}:</strong> ${vehicleData[key]}</p>`;
+          }
+
+          if (key === "data" && typeof vehicleData.data === "object") {
+            resultsHTML +=
+              '<div class="vehicle-data-nested"><h3>Additional Details</h3>';
+            Object.keys(vehicleData["data"]).forEach((nestedKey) => {
+              const nestedValue = vehicleData["data"][nestedKey];
+              // Handle recall information if present
+              if (
+                nestedKey === "RecallInfo" &&
+                Array.isArray(vehicleData["data"]["RecallInfo"])
+              ) {
+                resultsHTML += '<div class="vehicle-recall-info">';
+                vehicleData["data"]["RecallInfo"].forEach((recall, i) => {
+                  resultsHTML += `<div class="recall-item"><h3>${
+                    i + 1
+                  }# RECALL</h3>`;
+                  Object.keys(recall).forEach((recallKey) => {
+                    resultsHTML += `<p><strong>${recallKey
+                      .replace(/([A-Z])/g, " $1")
+                      .trim()}:</strong> ${JSON.stringify(
+                      recall[recallKey]
+                    )}</p>`;
+                  });
+                  resultsHTML += `</div>`; // End of recall-item
+                });
+                resultsHTML += "</div>"; // End of vehicle-recall-info
+              }
+
+              if (nestedKey !== "RecallInfo") {
+                if (typeof nestedValue !== "object") {
+                  resultsHTML += `<p><strong>${getCustomKey(nestedKey)
+                    .replace(/([A-Z])/g, " $1")
+                    .trim()}:</strong> ${nestedValue}</p>`;
+                } else {
+                  // Here you could add further nesting, or convert the object to a JSON string
+                  resultsHTML += `<p><strong>${getCustomKey(nestedKey)
+                    .replace(/([A-Z])/g, " $1")
+                    .trim()}:</strong> ${JSON.stringify(nestedValue)}</p>`;
+                }
+              }
+            });
+            resultsHTML += "</div>"; // End of vehicle-data-nested
+          }
+        });
+
+        resultsHTML += "</div>"; // End of vehicle-data
       }
 
-      const filteredResults = data.filter(
-        (result) => !result.Variable.includes("Error") && result.Value
-      );
-
-      let resultsHTML = `
-  <table class="vin-results-table">
-    <thead>
-      <tr class="header-row">
-        <th>Attribute</th>
-        <th>Value</th>
-      </tr>
-    </thead>
-    <tbody>
-`;
-
-      filteredResults.forEach((result, index) => {
-        resultsHTML += `
-    <tr class="${index % 2 === 0 ? "even-row" : "odd-row"}">
-      <td class="attribute-cell">${result.Variable}</td>
-      <td class="value-cell">${result.Value}</td>
-    </tr>
-  `;
-      });
-
-      resultsHTML += `</tbody></table>`;
+      // Display the results
       resultsDiv.innerHTML = resultsHTML;
     } catch (error) {
-      // If the error object has a message, display that, otherwise use a default error message
-      resultsDiv.textContent = error.message || "Error fetching VIN data.";
+      console.error("Error fetching VIN data.", error);
+      resultsDiv.innerHTML = `<p>Error fetching data. Please try again later.</p>`;
     }
-
-    const vinLink = `https://www.vindecoderfree.com/vin/${vin}`;
-    document.getElementById(
-      "linkDiv"
-    ).innerHTML = `<a href="${vinLink}" target="_blank">View full details on VinDecoderFree.com</a>`;
-  });
+  }
+});
